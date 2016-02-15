6865f4311de86bbc70ad910516bac4606dc9c1a4
kevinkbc <c.kevinbarros@gmail.com>
2016-02-15 13:07:13 -0300

    mudanças no bankControllerTest

--- test/test/controller/BankControllerTest.java
+++ test/test/controller/BankControllerTest.java
@@ -2,6 +2,7 @@ package test.controller;
 
 import static org.junit.Assert.*;
 
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -10,6 +11,7 @@ import banksys.account.AbstractAccount;
 import banksys.account.OrdinaryAccount;
 import banksys.account.SavingsAccount;
 import banksys.account.SpecialAccount;
+import banksys.account.TaxAccount;
 import banksys.control.BankController;
 import banksys.control.exception.BankTransactionException;
 import banksys.persistence.AccountVector;
@@ -22,15 +24,30 @@ import banksys.persistence.exception.AccountNotFoundException;
 public class BankControllerTest {
 	
 	static BankController bank;
-	static AbstractAccount acc;
+	static AbstractAccount acc, ordinaryAccount, savingsAccount, taxAccount, specialAccount;
 	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		bank = new BankController(new SQLiteAccounts());
-		acc = new OrdinaryAccount("1");
-		bank.addAccount(acc);
+		ordinaryAccount = new OrdinaryAccount("101");
+		specialAccount = new SpecialAccount("102");
+		savingsAccount = new SavingsAccount("103");
+		taxAccount = new TaxAccount("104");
+		//acc = new OrdinaryAccount("105");
+		//bank.addAccount(acc);
 	}
 
+	@AfterClass
+	public static void tearDownAfterClass(){
+		try{
+			bank.removeAccount(ordinaryAccount.getNumber());
+			bank.removeAccount(specialAccount.getNumber());
+			bank.removeAccount(savingsAccount.getNumber());
+			bank.removeAccount(taxAccount.getNumber());
+		} catch(Exception e){
+			
+		}
+	}
 	/*@Test
 	public void addAcountTest() {
 		
@@ -46,11 +63,11 @@ public class BankControllerTest {
 	@Test
 	public void doCreditTest() {
 		try {
-			bank.doCredit("1", 10);
-			double result = bank.getBalance("1");
+			String number = ordinaryAccount.getNumber();
+			bank.doCredit(number, 10);
+			double result = bank.getBalance(number);
 			assertEquals(10.0, result, 0);
 		} catch (BankTransactionException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
@@ -58,11 +75,11 @@ public class BankControllerTest {
 	@Test
 	public void doDebitTest() {
 		try {
-			bank.doDebit("1", 5);
-			double result = bank.getBalance("1");
+			String number = ordinaryAccount.getNumber();
+			bank.doDebit(number, 5);
+			double result = bank.getBalance(number);
 			assertEquals(5, result, 0);
 		} catch (BankTransactionException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
@@ -70,10 +87,9 @@ public class BankControllerTest {
 	@Test
 	public void getBalanceTest() {
 		try {
-			double result = bank.getBalance("1");
+			double result = bank.getBalance(savingsAccount.getNumber());
 			assertEquals(0, result, 0);
 		} catch (BankTransactionException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
@@ -81,29 +97,30 @@ public class BankControllerTest {
 	@Test
 	public void doTransferTest() {
 		try {
-			AbstractAccount acc2 = new OrdinaryAccount("2");
-			bank.addAccount(acc2);
-			bank.doCredit("1", 10);
-			bank.doTransfer("1", "2", 10);
-			double result = bank.getBalance("2");
+			//AbstractAccount acc2 = new OrdinaryAccount("2");
+			//bank.addAccount(acc2);
+			String numberFrom = ordinaryAccount.getNumber();
+			String numberTo = taxAccount.getNumber();
+			bank.doCredit(numberFrom, 10);
+			bank.doTransfer(numberFrom, numberTo, 10);
+			double result = bank.getBalance(numberTo);
 			assertEquals(10, result, 0);
 		} catch (BankTransactionException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	
 	@Test
 	public void doEarnBonusTest(){
-		AbstractAccount acc2 = new SpecialAccount("3");
+		//AbstractAccount acc2 = new SpecialAccount("3");
+		String number = specialAccount.getNumber();
 		try {
-			bank.addAccount(acc2);
-			bank.doCredit("3", 10);
-			bank.doEarnBonus("3");
-			double result = bank.getBalance("3");
+			//bank.addAccount(acc2);
+			bank.doCredit(number, 10);
+			bank.doEarnBonus(number);
+			double result = bank.getBalance(number);
 			assertEquals(10.1, result, 0);
 		} catch (BankTransactionException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
@@ -111,12 +128,13 @@ public class BankControllerTest {
 	
 	@Test
 	public void doEarnInterestTest(){
-		AbstractAccount acc2 = new SavingsAccount("4");
+		//AbstractAccount acc2 = new SavingsAccount("4");
+		String number = savingsAccount.getNumber();
 		try {
-			bank.addAccount(acc2);
-			bank.doCredit("4", 10);
-			bank.doEarnInterest("4");
-			double result = bank.getBalance("4");
+			//bank.addAccount(acc2);
+			bank.doCredit(number, 10);
+			bank.doEarnInterest(number);
+			double result = bank.getBalance(number);
 			assertEquals(10.01, result, 0);
 		} catch (BankTransactionException e) {
 			// TODO Auto-generated catch block
